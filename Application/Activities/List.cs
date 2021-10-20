c785c1d755a22f9ca14df79fecc90740f77953b7
Adrian Salatino <Adrian.Salatino.-ND@disney.com>
2021-10-20 16:31:22 -0300

    End of section 4

---
+++ Application/Activities/List.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Domain;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Persistence;
+
+namespace Application.Activities
+{
+    public class List
+    {
+        public class Query : IRequest<List<Activity>> { }
+
+        public class Handler : IRequestHandler<Query, List<Activity>>
+        {
+            private readonly DataContext _context;
+            public Handler(DataContext context)
+            {
+                _context = context;
+            }
+
+            public async Task<List<Activity>> Handle(Query request, CancellationToken cancellationToken)
+            {
+                // try
+                // {
+                //     for (var i = 0; i < 10; i++)
+                //     {
+                //         cancellationToken.ThrowIfCancellationRequested();
+                //         await Task.Delay(1000, cancellationToken);
+                //         _logger.LogInformation($"Task {i} has completed");
+                //     }
+                // }
+                // catch (Exception ex) when (ex is TaskCanceledException)
+                // {
+                //     _logger.LogInformation("Task was cancelled");
+                // }
+
+                return await _context.Activities.ToListAsync();
+            }
+        }
+    }
+}
