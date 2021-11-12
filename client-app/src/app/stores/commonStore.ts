2dd0604c76b20ce7e9342e8e58d943d7a277e62e
Adrian Salatino <Adrian.Salatino.-ND@disney.com>
2021-11-12 17:22:23 -0300

    150

--- client-app/src/app/stores/commonStore.ts
+++ client-app/src/app/stores/commonStore.ts
@@ -3,6 +3,8 @@ import { ServerError } from "../models/serverError";
 
 export default class CommonStore {
     error: ServerError | null = null;
+    token: string | null = null;
+    appLoaded = false;
 
     constructor() {
         makeAutoObservable(this);        
@@ -12,4 +14,12 @@ export default class CommonStore {
         this.error = error;
     }
     
+    setToken = (token: string | null) => {
+        if (token) window.localStorage.setItem('jwt', token);
+        this.token = token;
+    }
+
+    setAppLoaded = () => {
+      this.appLoaded = true;
+    }
 }
--- client-app/src/features/home/HomePage.tsx
+++ client-app/src/features/home/HomePage.tsx
@@ -1,20 +1,31 @@
+import { observer } from "mobx-react-lite";
 import React from "react";
 import { Link } from "react-router-dom";
 import { Container, Header, Segment, Image, Button } from "semantic-ui-react";
+import { useStore } from "../../app/stores/store";
 
-export default function HomePage(){
+export default observer(function HomePage() {
+    const { userStore } = useStore();
     return (
         <Segment inverted textAlign='center' vertical className='masthead' >
             <Container text >
                 <Header as='h1' inverted >
-                    <Image size='massive' src='/assets/logo.png' alt='logo' style={{marginBottom: 12}} />
+                    <Image size='massive' src='/assets/logo.png' alt='logo' style={{ marginBottom: 12 }} />
                     Reactivities
                 </Header>
-                <Header as='h2' inverted content='Welcome to Reactivities' />
-                <Button as={Link} to='/login' size='huge' inverted >
-                    Login!
-                </Button>
+                {userStore.isLoggedIn ? (
+                    <>
+                        <Header as='h2' inverted content='Welcome to Reactivities' />
+                        <Button as={Link} to='/activities' size='huge' inverted >
+                            Go to Activities!
+                        </Button>
+                    </>
+                ) : (
+                    <Button as={Link} to='/login' size='huge' inverted >
+                        Login!
+                    </Button>
+                )}
             </Container>
         </Segment>
     )
-}
+})
