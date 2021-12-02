1ed3dae74bb8e707a217124587556e303435d8db
Adrian Salatino <Adrian.Salatino.-ND@disney.com>
2021-12-02 00:20:04 -0300

    Added blob CSP

--- API/Startup.cs
+++ API/Startup.cs
@@ -55,7 +55,7 @@ namespace API
                 .FontSources(s => s.Self().CustomSources("https://fonts.gstatic.com", "data:"))
                 .FormActions(s => s.Self())
                 .FrameAncestors(s => s.Self())
-                .ImageSources(s => s.Self().CustomSources("https://res.cloudinary.com"))
+                .ImageSources(s => s.Self().CustomSources("https://res.cloudinary.com", "blob:"))
                 .ScriptSources(s => s.Self().CustomSources("sha256-k3MyskE/gtLRpT9GNYIeCZevL3K83+ilwjQ8T+P4yW4="))
             );
 
